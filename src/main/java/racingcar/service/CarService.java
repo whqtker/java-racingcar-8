4e32132db7a5ecdec4ff88289b4e21ac4aacc158
seonghyeok <chosh2001@naver.com>
2025-10-25 21:32:14 +0900

    refactor: 중복된 자동차 이름 입력 관련 검증 추가

--- src/main/java/racingcar/service/CarService.java
+++ src/main/java/racingcar/service/CarService.java
@@ -1,7 +1,9 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import racingcar.domain.Car;
 import racingcar.repository.CarRepository;
 
@@ -14,8 +16,18 @@ public class CarService {
     }
 
     public void registerCars(List<String> names) {
+        validateDuplicatedNames(names);
         for (String name : names) {
             carRepository.addCar(new Car(name, 0));
+        }
+    }
+
+    private void validateDuplicatedNames(List<String> names) {
+        Set<String> distinctNames = new HashSet<>();
+        for (String name : names) {
+            if (!distinctNames.add(name)) {
+                throw new IllegalArgumentException("중복된 자동차 이름이 있습니다.");
+            }
         }
     }
 
